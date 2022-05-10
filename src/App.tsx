d9b33078b1f57ad27d370b9292cf867ff676ebf3
Kaja Anna <kaja.szokalska@gmail.com>
2022-05-10 20:11:29 +0200

    set routes + add fav icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 import './App.sass';
 
 const App = () => {
   return (
     <div className="App">
+      <Routes >
+        <Route path="/" element="MainPage" />
+        <Route path="/:usesId" element="UsersBudgets"/>
+        <Route path="/:usesId/:budgetId" element="BudgetSummary"/>
+        <Route path="/createBudget" element="CreateBudget"/>
+        <Route path="/options" element="Options"/>
+      </Routes>
      
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.sass';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+
+// import store from 
+
 import App from './App';
+
+import './index.sass';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
